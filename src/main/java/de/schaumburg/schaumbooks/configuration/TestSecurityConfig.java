f9cf4007fc5885d35542c0003a2e748c8eae6229
Sven Schroeder <schuen@gmx.de>
2025-01-10 07:36:25 +0100

    Add UserDetailsService in Config

--- src/main/java/de/schaumburg/schaumbooks/configuration/TestSecurityConfig.java
+++ src/main/java/de/schaumburg/schaumbooks/configuration/TestSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,10 +25,25 @@ public class TestSecurityConfig {
 
         // ensures that every request requires authentication
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-        // disable form login since not using login page
-            // .formLogin(Customizer.withDefaults())
-            .httpBasic(Customizer.withDefaults());
+                // disable form login since not using login page
+                // .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("sven")
+                .password("1234")
+                // .roles("USER")
+                .build();
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("hans")
+                .password("3456")
+                // .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
