976d563dbc2ac5de642f5ae96f81dd2fd7ef2ed0
Sven Schroeder <schuen@gmx.de>
2025-02-01 14:05:34 +0100

    make security filter chain readable

--- src/main/java/de/schaumburg/schaumbooks/configuration/TestSecurityConfig.java
+++ src/main/java/de/schaumburg/schaumbooks/configuration/TestSecurityConfig.java
@@ -17,19 +17,22 @@ public class TestSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // disable csrf since making the API stateless
-        http.csrf(csrfConfigCustomizer -> csrfConfigCustomizer.disable());
-        // this means every request for every endpoint is permitted
-        // disable for now to implement security
-        // http.authorizeHttpRequests(request -> request.anyRequest().permitAll());
+        return http
+        // first you declare WHAT you want to protect and how:
+            .authorizeHttpRequests(
+                authorizeHttp -> {
+                    // ensures that every request requires authentication
+                    // except thows you declare here with permitAll();
+                    authorizeHttp.anyRequest().authenticated();
 
-        // ensures that every request requires authentication
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                // disable form login since not using login page
-                // .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                }
+            )
+        // secondly you declare HOW you want to login:
+            // disable csrf since making the API stateless
+            .csrf(csrfConfigCustomizer -> csrfConfigCustomizer.disable())
+            .httpBasic(Customizer.withDefaults())
+            .build();
 
-        return http.build();
     }
 
     @Bean
