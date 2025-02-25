77be5058097a4ad81d1d5da438900791b9cd3911
Sven Schroeder <schuen@gmx.de>
2025-02-25 20:30:46 +0100

    add patch user in controller

--- src/main/java/de/schaumburg/schaumbooks/user/UserController.java
+++ src/main/java/de/schaumburg/schaumbooks/user/UserController.java
@@ -1,12 +1,14 @@
 package de.schaumburg.schaumbooks.user;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +53,11 @@ public class UserController {
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody @Valid User user) {
         User updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> updateUserFields(@PathVariable Long id, @RequestBody Map<String, Object> fieldsToPatch){
+        User updatedUser = userService.updateUserFields(id, fieldsToPatch);
         return ResponseEntity.ok(updatedUser);
     }
 
--- src/test/java/de/schaumburg/schaumbooks/user/UserControllerTest.java
+++ src/test/java/de/schaumburg/schaumbooks/user/UserControllerTest.java
@@ -1,11 +1,15 @@
 package de.schaumburg.schaumbooks.user;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -57,9 +61,9 @@ public class UserControllerTest {
     public void setup() {
 
         users = List.of(new User(1l, "user1", "1234", "Test1", "Test1", "test@email.com", List.of(Role.STUDENT), "8a"),
-                        new User(2l, "user2", "1234", "Test2", "Test2", "test2@email.com", List.of(Role.STUDENT), "8b"));
-                        new User(4l, "user3", "1234", "Horst", "admin", "admin@email.com", List.of(Role.ADMIN), null);
-                        new User(3l, "user4", "1234", "Hans", "teacher", "teacher1@email.com", List.of(Role.TEACHER), null);
+                new User(2l, "user2", "1234", "Test2", "Test2", "test2@email.com", List.of(Role.STUDENT), "8b"));
+        new User(4l, "user3", "1234", "Horst", "admin", "admin@email.com", List.of(Role.ADMIN), null);
+        new User(3l, "user4", "1234", "Hans", "teacher", "teacher1@email.com", List.of(Role.TEACHER), null);
     }
 
     // GET: findAll
@@ -110,14 +114,14 @@ public class UserControllerTest {
     }
 
     @Test
-    void shouldFindUserGivenUsername()throws Exception{
+    void shouldFindUserGivenUsername() throws Exception {
         // Given
         String username = "user1";
         String jsonResponse = objectMapper.writeValueAsString(users.get(0));
         // When
         when(userService.findUserByUsername(username)).thenReturn(users.get(0));
         // Then
-        mockMvc.perform(get("/api/users/username/"+username)
+        mockMvc.perform(get("/api/users/username/" + username)
                 .with(httpBasic("sven", "1234")))
                 .andExpect(status().isOk())
                 .andExpect(content().json(jsonResponse));
@@ -152,24 +156,30 @@ public class UserControllerTest {
 
     private static Stream<Arguments> provideInvalidStudents() {
         return Stream.of(
-                Arguments.of(new User(null,"user20", "1234", "", "Doe", "john.doe@example.com", List.of(Role.STUDENT), "10a")), // empty
-                                                                                                               // firstName
-                Arguments.of(new User(null, "user21", "1234", "John", "", "john.doe@example.com", List.of(Role.STUDENT), "10b")), // empty
-                                                                                                                // lastName
-                // Arguments.of(new User(null, "John", "Doe", "john.doe@example.com", List.of(Role.STUDENT), "")), // empty
-                                                                                                                // className
-                                                                                                                // for
-                                                                                                                // student
-                Arguments.of(new User(null,"user22", "1234",  "John", "Doe", "", List.of(Role.STUDENT), "10A")), // empty email
-                Arguments.of(new User(null,"user23", "1234", "John", "Doe", "not-an-email", List.of(Role.STUDENT), "10A")) // invalid
-                                                                                                          // email
+                Arguments.of(new User(null, "user20", "1234", "", "Doe", "john.doe@example.com", List.of(Role.STUDENT),
+                        "10a")), // empty
+                // firstName
+                Arguments.of(new User(null, "user21", "1234", "John", "", "john.doe@example.com", List.of(Role.STUDENT),
+                        "10b")), // empty
+                // lastName
+                // Arguments.of(new User(null, "John", "Doe", "john.doe@example.com",
+                // List.of(Role.STUDENT), "")), // empty
+                // className
+                // for
+                // student
+                Arguments.of(new User(null, "user22", "1234", "John", "Doe", "", List.of(Role.STUDENT), "10A")), // empty
+                                                                                                                 // email
+                Arguments.of(
+                        new User(null, "user23", "1234", "John", "Doe", "not-an-email", List.of(Role.STUDENT), "10A")) // invalid
+        // email
         );
     }
 
     // PUT: updateStudent()
     @Test
     void shouldUpdateStudentGivenValidInput() throws JsonProcessingException, Exception {
-        User student = new User(1L,"user1", "1234",  "updatedName", "Test", "Hans@email.com", List.of(Role.STUDENT), "8b");
+        User student = new User(1L, "user1", "1234", "updatedName", "Test", "Hans@email.com", List.of(Role.STUDENT),
+                "8b");
 
         when(userService.updateUser(1l, student)).thenReturn(student);
 
@@ -183,7 +193,8 @@ public class UserControllerTest {
 
     @Test
     void shouldGetStudentNotFoundGivenInvalidId() throws JsonProcessingException, Exception {
-        User student = new User(999L, "user999", "1234", "updatedName", "Test", "Hans@email.com", List.of(Role.STUDENT), "8b");
+        User student = new User(999L, "user999", "1234", "updatedName", "Test", "Hans@email.com", List.of(Role.STUDENT),
+                "8b");
 
         when(userService.updateUser(999L, student)).thenThrow(UserNotFoundException.class);
 
@@ -215,18 +226,32 @@ public class UserControllerTest {
                 // .andExpect(jsonPath("$.className").value("must not be empty"))
                 .andExpect(jsonPath("$.email").value("must be a well-formed email address"));
     }
-    // TODO: finish test
+
     @Test
-    void shouldPatchUserFieldsSuccessfully(){
+    void shouldPatchUserFieldsSuccessfully() throws JsonProcessingException, Exception {
         // Given
-        Map<String,Object> updateFields = Map.of(
-            "firstName", "NewName",
-            "email", "new.email@example.com"
+        Map<String, Object> updateFields = Map.of(
+                "firstName", "NewName",
+                "email", "new.email@example.com"
 
         );
+        User updatedUser = new User(1l, "user1", "1234", "NewName", "Test1", "new.email@example.com",
+                List.of(Role.STUDENT), "8a");
+        // When
+        when(userService.updateUserFields(eq(1L), any(Map.class))).thenReturn(updatedUser);
+        // When/Then
+        mockMvc.perform(patch("/api/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(updateFields))
+                .with(httpBasic("sven", "1234")))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("NewName"))
+                .andExpect(jsonPath("$.email").value("new.email@example.com"));
 
-        User updatedUser = users.get(0);
+        verify(userService, times(1)).updateUserFields(eq(1L), any(Map.class));
     }
+
+    // TODO: test for failing patch request
 
     // DELETE: deleteStudentById()
     @Test
