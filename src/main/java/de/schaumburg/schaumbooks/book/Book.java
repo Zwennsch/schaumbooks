8eb5fe7010db0332fece723faf2d05c1870d6244
Sven Schroeder <schuen@gmx>
2025-02-28 11:57:06 +0100

    switch to book record

--- src/main/java/de/schaumburg/schaumbooks/book/Book.java
+++ src/main/java/de/schaumburg/schaumbooks/book/Book.java
@@ -11,146 +11,20 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotEmpty;
 
-
-// Try to switch to record:
 @Entity
-public class Book {
-        private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
-        private @NotEmpty String title;
-        private @NotEmpty String verlag;
-        private @NotEmpty String isbn;
-        @Enumerated(EnumType.STRING)
-        private BookStatus status = BookStatus.AVAILABLE;
-        // TODO: this should be named as user instead of student
-        @ManyToOne
-        @JoinColumn(name = "user_id")
-        private User user;
+public record Book(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id,
+    @NotEmpty String title,
+    @NotEmpty String verlag,
+    @NotEmpty String isbn,
+    @Enumerated(EnumType.STRING) BookStatus status,
+    @ManyToOne @JoinColumn(name = "user_id") User user
+) {
+    public Book() {
+        this(null, "", "", "", BookStatus.AVAILABLE, null);
+    }
 
-        public Book(Long id, String title, String verlag, String isbn, BookStatus status,
-                        User user) {
-                this.id = id;
-                this.title = title;
-                this.verlag = verlag;
-                this.isbn = isbn;
-                this.status = status;
-                this.user = user;
-        }
-
-        Book() {
-
-        }
-
-        public Boolean isLend() {
-                if (this.status == BookStatus.LENT)
-                        return true;
-                return false;
-        }
-
-        public Long getId() {
-                return id;
-        }
-
-        public void setId(Long id) {
-                this.id = id;
-
-        }
-
-        public String getTitle() {
-                return title;
-        }
-
-        public void setTitle(String title) {
-                this.title = title;
-        }
-
-        public String getIsbn() {
-                return isbn;
-        }
-
-        public void setIsbn(String isbn) {
-                this.isbn = isbn;
-        }
-
-        public BookStatus getStatus() {
-                return status;
-        }
-
-        public void setStatus(BookStatus status) {
-                this.status = status;
-        }
-
-        public void setUser(User user) {
-                this.user = user;
-        }
-
-        public User getUser() {
-                return user;
-        }
-
-        public String getVerlag() {
-                return verlag;
-        }
-
-        public void setVerlag(String verlag) {
-                this.verlag = verlag;
-        }
-
-        @Override
-        public int hashCode() {
-                final int prime = 31;
-                int result = 1;
-                result = prime * result + ((id == null) ? 0 : id.hashCode());
-                result = prime * result + ((title == null) ? 0 : title.hashCode());
-                result = prime * result + ((verlag == null) ? 0 : verlag.hashCode());
-                result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
-                result = prime * result + ((status == null) ? 0 : status.hashCode());
-                result = prime * result + ((user == null) ? 0 : user.hashCode());
-                return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-                if (this == obj)
-                        return true;
-                if (obj == null)
-                        return false;
-                if (getClass() != obj.getClass())
-                        return false;
-                Book other = (Book) obj;
-                if (id == null) {
-                        if (other.id != null)
-                                return false;
-                } else if (!id.equals(other.id))
-                        return false;
-                if (title == null) {
-                        if (other.title != null)
-                                return false;
-                } else if (!title.equals(other.title))
-                        return false;
-                if (verlag == null) {
-                        if (other.verlag != null)
-                                return false;
-                } else if (!verlag.equals(other.verlag))
-                        return false;
-                if (isbn == null) {
-                        if (other.isbn != null)
-                                return false;
-                } else if (!isbn.equals(other.isbn))
-                        return false;
-                if (status != other.status)
-                        return false;
-                if (user == null) {
-                        if (other.user != null)
-                                return false;
-                } else if (!user.equals(other.user))
-                        return false;
-                return true;
-        }
-
-        @Override
-        public String toString() {
-                return "Book [id=" + id + ", title=" + title + ", verlag=" + verlag + ", isbn=" + isbn + ", status="
-                                + status + ", user=" + user + "]";
-        }
-
+    public Boolean isLend() {
+        return this.status == BookStatus.LENT;
+    }
 }
